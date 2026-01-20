a601b7f4f09be04cdda7af8683f87c7e8ff64c8a
Reetika-Chavan <reetika.chavan@contentstack.com>
2026-01-20 14:07:46 +0530

    Test deployment #3 - 2026-01-20 14:07:46

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -24,3 +24,4 @@ export default async function handler(request, context) {
 // Deployment test #1 - 2026-01-20 13:50:47
 // Deployment test #1 - 2026-01-20 13:59:43
 // Deployment test #2 - 2026-01-20 14:03:45
+// Deployment test #3 - 2026-01-20 14:07:46
